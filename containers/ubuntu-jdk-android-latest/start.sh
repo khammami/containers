c09205bcdd9c123e03afe7c97819209d556b344d
khammami <khammami@gmail.com>
2024-02-14 21:20:17 +0100

    Remove user

--- containers/ubuntu-jdk-android-latest/start.sh
+++ containers/ubuntu-jdk-android-latest/start.sh
@@ -3,5 +3,5 @@
 # It is workaround to access adb from the created user
 echo "Prepare adb to have access to device"
 sudo /opt/android/platform-tools/adb devices >/dev/null
-sudo chown -R 1300:1301 .android
+# sudo chown -R 1300:1301 .android
 echo "adb can be used now"
